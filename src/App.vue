c49de529340aabd3ae9451f2efb1a8c70947235f
wwtfly <wwtfly@gmail.com>
2019-07-17 07:07:31 +0800

    修改element UI 默认样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <PadInfo :title_message="title_message" :message="message"/>
-    <ContainerInfo/>
+    <PadInfo :title_message="title_message" :message="message" />
+    <ContainerInfo />
   </div>
 </template>
 
@@ -20,9 +20,9 @@ export default {
       title_message:"欢迎使用,这是新开发的插件",
       message:{
             新手指南:{
-                  百度:"http://www.baidu.com",
-                  淘宝:"http://www.taobao.com",
-                  京东:"http://www.jd.com"
+                  百度百度百度百度百度:"http://www.baidu.com",
+                  淘宝淘宝淘宝淘宝淘宝淘宝淘宝:"http://www.taobao.com",
+                  京东京东京东京东京东京东:"http://www.jd.com"
             },
             常见问题:{
                   百:"http://www.baidu.com",
--- src/plugin/PadInfo.vue
+++ src/plugin/PadInfo.vue
@@ -1,29 +1,39 @@
 <template>
-    <div id="my-pad">
-        <div id="my-info">
-            <span>通知</span>
-                <el-carousel 
-                indicator-position="none"
-                height="30px"
-                :interval=5000
-                arrow=never
-                >
-                    <!-- <el-carousel-item v-for="item in items" :key="item"> -->
-                    <el-carousel-item v-for="(item,index) in items" :key="index">
-                        <span>{{ item }}</span>
-                    </el-carousel-item>
-                </el-carousel>
-        </div>
-        <div id="my-content">
-            <div class="pad" v-for="(value,key,index) in this.message" :key=index>
-                <span>{{key}}</span>
-            <ul>
-                <li v-for="(value,key,i) in value" :key=i><a :href=value>{{key}}</a></li>
-            </ul>
-            </div>
-        </div>
-        <!-- <input v-model="message"/> -->
+  <div id="my-pad">
+    <div id="my-info">
+      <span>通知</span>
+      <el-carousel
+        indicator-position="none"
+        height="30px"
+        :interval="5000"
+        arrow="never"
+      >
+        <!-- <el-carousel-item v-for="item in items" :key="item"> -->
+        <el-carousel-item v-for="(item, index) in items" :key="index">
+          <span>{{ item }}</span>
+        </el-carousel-item>
+      </el-carousel>
     </div>
+    <div id="my-content">
+      <div class="pad" v-for="(value, key, index) in this.message" :key="index">
+        <span>{{ key }}</span>
+        <ul>
+          <li v-for="(value, key, i) in value" :key="i">
+            <el-tooltip
+              popper-class="test"
+              effect="dark"
+              placement="top-start"
+              visible-arrow
+            >
+              <span slot="content">{{ key }}</span>
+              <a :href="value">{{ key }}</a>
+            </el-tooltip>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- <input v-model="message"/> -->
+  </div>
 </template>
 
 <script>
@@ -42,66 +52,97 @@ export default {
 }
 </script>
 
-<style scoped>
-body,html{
-    font-family: Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,SimSun,sans-serif;
+<style>
+body,
+html {
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
 }
 #my-pad {
-    width: auto;
-    height: 120px;
-    font-size: 14px;
-    background:transparent;
+  width: auto;
+  height: 120px;
+  font-size: 14px;
+  background: transparent;
 
-    /* background-color: #f9fafc; */
+  /* background-color: #f9fafc; */
 }
 #my-info {
-    position: relative;
-    height: 40px;
+  position: relative;
+  height: 40px;
 }
-#my-info span{
-    position: relative;
-    float: left;
-    line-height: 40px;
-    margin-left:10px;
-    color:#f54747;
-    font-weight: 500;
+#my-info span {
+  position: relative;
+  float: left;
+  line-height: 40px;
+  margin-left: 10px;
+  color: #f54747;
+  font-weight: 500;
 }
-#my-content{
-    position: relative;
-    padding-left: 10px;
+#my-content {
+  position: relative;
+  padding-left: 10px;
 }
-.pad{
-    position: relative;
-    float: left;
-    width:200px;
-    margin-right:5px;
-    height:100px;
+.pad {
+  position: relative;
+  float: left;
+  width: 200px;
+  margin-right: 5px;
+  height: 100px;
 }
-.pad span{
-    text-align: center;
-    display: block;
-    width:200px;
-    color:#5e6d82
+.pad span {
+  text-align: center;
+  display: block;
+  width: 200px;
+  color: #5e6d82;
 }
-.pad ul{
-    width:200px;
-    border-right: solid 1px #dcdfe6;
-    margin: 0px;
-    padding:0px;
+.pad ul {
+  width: 200px;
+  border-right: solid 1px #dcdfe6;
+  margin: 0px;
+  padding: 0px;
 }
-.pad ul li{
-    display: inline;
-    margin-right: 10px;
-    list-style: none;
-    /* padding: 0px 20px; */
+.pad ul li {
+  display: inline-block;
+  width: 80px;
+  margin-right: 10px;
+  list-style: none;
+  /* 强制不换行 */
+  white-space: nowrap;
+  /* 自动隐藏 */
+  overflow: hidden;
+  /* 文字隐藏后自动添加省略号 */
+  text-overflow: ellipsis;
+  color: #409eff;
+
+  /* padding: 0px 20px; */
 }
 .pad ul li a {
-    text-decoration: none;
-    color:#409eff;
+  text-decoration: none;
+  color: #409eff;
 }
 /* #my-info div{
     position: relative;
     float: left;
     height: 40px;
+} */
+.test {
+  background: #f3f5f3 !important;
+  box-shadow: 3px 2px 2px 1px #e2e0e0 !important;
+  border-radius: 1px !important;
+  color: #5e6d82 !important;
+}
+/* .el-tooltip__popper[x-placement^='top'] {
+  background: #1ab394;
+  color: #fff;
+}
+.el-tooltip__popper {
+  position: absolute;
+  border-radius: 4px;
+  padding: 10px;
+  z-index: 2000;
+  font-size: 12px;
+  line-height: 1.2;
+  min-width: 10px;
+  word-wrap: break-word;
 } */
 </style>
